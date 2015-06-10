b1788bdbf5f5d12e28769a2a97f60a9e92fa2ae4
sebastian <sebastian.burgstaller@gmail.com>
2015-06-09 17:36:31 -0700

    wikidata properties dicts added

---
+++ wd_property_store.py
@@ -0,0 +1,142 @@
+__author__ = 'Sebastian Burgstaller'
+__licence__ = 'GPLv3'
+
+
+wd_properties = {
+    'P715': {
+        'datatype': 'string',
+        'name': 'Drugbank ID',
+        'domain': ['drugs']
+    },
+    'P279': {
+        'datatype': 'item',
+        'name': 'subclass of',
+        'domain': ['generic']
+    },
+    'P31': {
+        'datatype': 'item',
+        'name': 'instance of',
+        'domain': ['generic']
+    },
+    'P636': {
+        'datatype': 'item',
+        'name': 'route of administration',
+        'domain': ['drugs']
+    },
+    'P267': {
+        'datatype': 'string',
+        'name': 'ATC code',
+        'domain': ['drugs']
+    },
+    'P231': {
+        'datatype': 'string',
+        'name': 'CAS registry number',
+        'domain': ['drugs']
+    },
+    'P486': {
+        'datatype': 'string',
+        'name': 'MeSH ID',
+        'domain': ['drugs', 'diseases']
+    },
+    'P672': {
+        'datatype': 'string',
+        'name': 'MeSH Code',
+        'domain': ['drugs']
+    },
+    'P662': {
+        'datatype': 'string',
+        'name': 'PubChem ID (CID)',
+        'domain': ['drugs']
+    },
+    'P661': {
+        'datatype': 'string',
+        'name': 'ChemSpider ID',
+        'domain': ['drugs']
+    },
+    'P652': {
+        'datatype': 'string',
+        'name': 'UNII',
+        'domain': ['drugs']
+    },
+    'P665': {
+        'datatype': 'string',
+        'name': 'KEGG ID',
+        'domain': ['drugs']
+    },
+    'P683': {
+        'datatype': 'string',
+        'name': 'ChEBI ID',
+        'domain': ['drugs']
+    },
+    'P274': {
+        'datatype': 'string',
+        'name': 'chemical formula',
+        'domain': ['drugs']
+    },
+    'P592': {
+        'datatype': 'string',
+        'name': 'ChEMBL ID',
+        'domain': ['drugs']
+    },
+    'P233': {
+        'datatype': 'string',
+        'name': 'SMILES',
+        'domain': ['drugs']
+    },
+    'P234': {
+        'datatype': 'string',
+        'name': 'InChI',
+        'domain': ['drugs']
+    },
+    'P235': {
+        'datatype': 'string',
+        'name': 'InChIKey',
+        'domain': ['drugs']
+    },
+    'P1805': {
+        'datatype': 'string',
+        'name': 'Word Health Organisation International Nonproprietary Name',
+        'domain': ['drugs']
+    },
+    'P657': {
+        'datatype': 'string',
+        'name': 'RTECS Number',
+        'domain': ['drugs']
+    },
+    'P699': {
+        'datatype': 'string',
+        'name': 'Disease Ontology ID',
+        'domain': ['diseases']
+    },
+    'P1550': {
+        'datatype': 'string',
+        'name': 'Orphanet ID',
+        'domain': ['diseases']
+    },
+    'P494': {
+        'datatype': 'string',
+        'name': 'ICD-10',
+        'domain': ['diseases']
+    },
+    'P493': {
+        'datatype': 'string',
+        'name': 'ICD-9',
+        'domain': ['diseases']
+    },
+    'P1395': {
+        'datatype': 'string',
+        'name': 'National Cancer Institute ID',
+        'domain': ['diseases']
+    },
+    'P1748': {
+        'datatype': 'string',
+        'name': 'NCI Thesaurus ID',
+        'domain': ['diseases']
+    }
+    # ,
+    # '': {
+    #     'datatype': 'string',
+    #     'name': '',
+    #     'domain': ['']
+    # }
+}
