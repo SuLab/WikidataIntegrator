e9a501f13b08df057f89483a980aa92c7164b1b3
Andra Waagmeester <andra@micelio.be>
2015-06-10 14:19:21 -0700

    Added PBB_login.py

---
+++ PBB_login.py
@@ -0,0 +1,23 @@
+__author__ = 'sebastian'
+
+"""
+login routine for Wikidata
+"""
+import urllib2
+import pprint
+import requests
+try: import simplejson as json
+except ImportError: import json # http://stackoverflow.com/a/712799/155046
+
+def login(user, password):
+    # Login to wikidata
+    baseurl = 'http://www.wikidata.org/w/'
+    login_params = '?action=login&lgname=%s&lgpassword=%s&format=json'%(user, password)
+    wdapi = 'http://www.wikidata.org/w/api.php?action=login'
+    r1= requests.post(baseurl+'api.php'+login_params)
+    return r1.json()
+    
+def prettyPrint(variable):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(variable)
+    
