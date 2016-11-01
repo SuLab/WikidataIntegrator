a87301d0921b66709aacdcb1e77276afb5de3538
Greg Stupp <gstupp@scripps.edu>
2016-11-01 15:16:11 -0700

    add database release/edition item creator

--- PBB_Helpers.py
+++ PBB_Helpers.py
@@ -1,10 +1,124 @@
+import datetime
 import xml.etree.ElementTree as ET
 from time import gmtime, strftime
 
 import requests
 
 from . import PBB_Core, PBB_login
-from . PBB_Core import WDItemEngine
+from .PBB_Core import WDItemEngine
+
+
+class Release(object):
+    """
+    Create a release item
+    Example: Ensembl Release 86 Q27613766
+
+    A relase item has the following required properties:
+        title: **user_defined**
+        description: **user_defined**
+        instance of (P31): edition (Q3331189)
+        edition or translation of (P629): **user_defined** (wd_item or string of database name)
+        edition number (P393): **user_defined** (str)
+    Optional properties:
+        archive URL (P1065): (str)
+        publication date (P577): (wd_date)
+
+    Example usage:
+    r = PBB_Helpers.Release("Ensembl Release 85", "Release 85 of Ensembl", "85", edition_of="Ensembl",
+                            archive_url="http://jul2016.archive.ensembl.org/",
+                            pub_date='+2016-07-01T00:00:00Z', date_precision=10)
+    r.create(login)
+
+    """
+
+    def __init__(self, title, description, edition, edition_of=None, edition_of_wdid=None, archive_url=None,
+                 pub_date=None, date_precision=None):
+        """
+
+        :param title: title of release item
+        :type title: str
+        :param description: description of release item
+        :type description: str
+        :param edition: edition number or unique identifier for the release
+        :type edition: str
+        :param edition_of: name of database. database wdid will automatically be looked up. Must pass either edition_of
+        or edition_of_wdid
+        :type edition_of: str
+        :param edition_of_wdid: wikidata id of database
+        :type edition_of_wdid: str
+        :param archive_url: (optional)
+        :type archive_url: str
+        :param pub_date: (optional) Datetime will be converted to str
+        :type pub_date: str or datetime
+        :param date_precision: (optional) passed to PBB_Core.WDTime as is
+        :type date_precision: int
+        """
+        self.title = title
+        self.description = description
+        self.edition = str(edition)
+        self.archive_url = archive_url
+        if isinstance(pub_date, datetime.date):
+            self.pub_date = pub_date.strftime('+%Y-%m-%dT%H:%M:%SZ')
+        else:
+            self.pub_date = pub_date
+        self.date_precision = date_precision
+
+        if edition_of and edition_of_wdid:
+            raise ValueError("must provide either edition_of or edition_of_wdid")
+        if edition_of:
+            self.edition_of_wdid = self.lookup_database(edition_of)
+        else:
+            self.edition_of_wdid = edition_of_wdid
+
+        self.statements = None
+        self.make_statements()
+
+    @staticmethod
+    def lookup_database(edition_of):
+        query = """SELECT ?item ?itemLabel WHERE {
+                    ?item wdt:P31 wd:Q4117139 .
+                    SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+                }"""
+        results = WDItemEngine.execute_sparql_query(query)
+        db_item_map = {x['itemLabel']['value']: x['item']['value'].replace('http://www.wikidata.org/entity/', '') for x
+                       in results['results']['bindings']}
+        if edition_of not in db_item_map:
+            raise ValueError("Database {} not found in wikidata. Please provide edition_of_wdid".format(edition_of))
+        return db_item_map[edition_of]
+
+    def release_exists(self):
+        edition_dict = id_mapper("P393", (("P629", self.edition_of_wdid), ("P31", "Q3331189")))
+        print("existing releases of {}: {}".format(self.edition_of_wdid, edition_dict))
+        return edition_dict.get(self.edition, False)
+
+    def make_statements(self):
+        s = []
+        # instance of edition
+        s.append(PBB_Core.WDItemID('Q3331189', 'P31'))
+        # edition or translation of
+        s.append(PBB_Core.WDItemID(self.edition_of_wdid, 'P629'))
+        # edition number
+        s.append(PBB_Core.WDString(self.edition, 'P393'))
+
+        if self.archive_url:
+            s.append(PBB_Core.WDUrl(self.archive_url, 'P1065'))
+
+        if self.pub_date:
+            s.append(PBB_Core.WDTime(self.pub_date, 'P577', precision=self.date_precision))
+
+        self.statements = s
+
+    def create(self, login, check_existing=True):
+        if check_existing:
+            re = self.release_exists()
+            if re:
+                print("Release already exists")
+                return re
+        item = PBB_Core.WDItemEngine(item_name=self.title, data=self.statements, domain="release")
+        item.set_label(self.title)
+        item.set_description(description=self.description, lang='en')
+        try_write(item, self.edition + ";" + self.edition_of_wdid, 'P393;P629', login)
+        return item.wd_item_id
 
 
 class PubmedStub(object):
