4c72ee2ca0107330f61e9c446f01f311b348efa1
sebastian <sebastian.burgstaller@gmail.com>
2016-12-09 18:22:52 -0800

    adapted imports

--- wikidataintegrator/wdi_fastrun.py
+++ wikidataintegrator/wdi_fastrun.py
@@ -1,6 +1,6 @@
 import copy
 
-from wikidataintegrator.wdi_core import WDBaseDataType, WDItemEngine
+from wikidataintegrator.wdi_core import *
 
 __author__ = 'Sebastian Burgstaller-Muehlbacher'
 __license__ = 'AGPLv3'
