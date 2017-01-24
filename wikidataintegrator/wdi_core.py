a7e986fc569f4fd3081489157134dd9d981800b2
sebastian <sebastian.burgstaller@gmail.com>
2017-01-24 10:39:10 -0800

    minor change in fastrun behaviour, for multithreading

--- wikidataintegrator/wdi_core.py
+++ wikidataintegrator/wdi_core.py
@@ -200,13 +200,14 @@ class WDItemEngine(object):
             self.data = []
 
     def init_fastrun(self):
-        for c in self.fast_run_store:
+        for c in WDItemEngine.fast_run_store:
             if c.base_filter == self.fast_run_base_filter:
                 self.fast_run_container = c
 
         if not self.fast_run_container:
             self.fast_run_container = FastRunContainer(base_filter=self.fast_run_base_filter,
                                                        base_data_type=WDBaseDataType, engine=WDItemEngine)
+            WDItemEngine.fast_run_store.append(self.fast_run_container)
 
         self.require_write = self.fast_run_container.check_data(self.data, append_props=self.append_value,
                                                                 cqid=self.wd_item_id)
