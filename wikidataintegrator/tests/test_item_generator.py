ff54b75c9694106e9cdf5a63a31a7f26e45a3bd8
sebastian <sebastian.burgstaller@gmail.com>
2017-02-22 00:36:02 -0800

    login parameter for item generator added

--- wikidataintegrator/tests/test_item_generator.py
+++ wikidataintegrator/tests/test_item_generator.py
@@ -1,5 +1,6 @@
 import unittest
 import pprint
+import sys
 from wikidataintegrator import wdi_core, wdi_fastrun, wdi_login
 
 __author__ = 'Sebastian Burgstaller-Muehlbacher'
@@ -11,7 +12,10 @@ class TestItemGenerator(unittest.TestCase):
     def test_item_generator(self):
         items = ['Q408883', 'Q5', 'P715']
 
-        item_instances = wdi_core.WDItemEngine.generate_item_instances(items=items, server='www.wikidata.org')
+        # login_obj = wdi_login.WDLogin(user='', '')
+        login_obj = None
+        item_instances = wdi_core.WDItemEngine.generate_item_instances(items=items, server='www.wikidata.org',
+                                                                       login=login_obj)
 
         for qid, item in item_instances:
             print(qid)
