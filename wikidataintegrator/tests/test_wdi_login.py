52e9011b1eaf16795aca01da42185f0620fb7000
Greg Stupp <gstupp@scripps.edu>
2017-06-27 17:13:59 -0700

    add print function for python2

--- wikidataintegrator/tests/test_wdi_login.py
+++ wikidataintegrator/tests/test_wdi_login.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 
 from wikidataintegrator import wdi_login
@@ -7,10 +8,9 @@ import os
 WDUSER = os.getenv("WDUSER")
 WDPASS = os.getenv("WDPASS")
 
+
 def test_login():
     if WDUSER and WDPASS:
         login = wdi_login.WDLogin(WDUSER, WDPASS)
     else:
         print("no WDUSER or WDPASS found in environment variables", file=sys.stderr)
-
-
