b8b6542d558a78e5cbc849f6940aad99e069ddb1
Greg Stupp <gstupp@scripps.edu>
2017-06-27 17:07:22 -0700

    need python 3.5 for JSONDecodeError, but it works on python2.7 also. add wdi_login test if environment variables are provided

---
+++ wikidataintegrator/tests/test_wdi_login.py
@@ -0,0 +1,16 @@
+import sys
+
+from wikidataintegrator import wdi_login
+import os
+
+# look for environment variables. if none set, don't do anything
+WDUSER = os.getenv("WDUSER")
+WDPASS = os.getenv("WDPASS")
+
+def test_login():
+    if WDUSER and WDPASS:
+        login = wdi_login.WDLogin(WDUSER, WDPASS)
+    else:
+        print("no WDUSER or WDPASS found in environment variables", file=sys.stderr)
+
+
