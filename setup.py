87f1eb7c5806a9c4d259be7b7c3cc9f5170e6dc5
Greg Stupp <gstupp@scripps.edu>
2017-07-31 18:07:36 -0700

    change setup.py to manually set version number. travis to release to pypi on tags

--- setup.py
+++ setup.py
@@ -1,47 +1,12 @@
 import os
 from setuptools import setup, find_packages
 from subprocess import check_output
-from subprocess import CalledProcessError
-
-setup_path = os.path.dirname(__file__)
-
-print("setup_path: {}".format(setup_path))
-
-
-def read(fname):
-    return open(os.path.join(setup_path, fname)).read()
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 0
-REPO_URL = 'https://github.com/sebotic/WikidataIntegrator'
+MICRO_VERSION = 461
 
-version_file = os.path.join(setup_path, 'VERSION')
-
-try:
-    MICRO_VERSION = int(check_output("git rev-list --count master", shell=True).decode('utf-8').strip('\n'))
-
-    f = open(version_file, 'w')
-    f.write("{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION))
-    f.close()
-except Exception as e:
-
-    print(e)
-    if os.path.isfile(version_file):
-        f = open(version_file, 'rt')
-        version_string = f.readline()
-        MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION = version_string.split('.')
-    else:
-        MICRO_VERSION = 1
-
-# Calculate commit hash
-try:
-    commit_hash = check_output("git rev-parse HEAD", shell=True).decode('utf-8').strip('\n')
-except CalledProcessError:
-    commit_hash = ''
-
-f = open('./.git-commit-hash', 'w')
-f.write("{}.git\n{}".format(REPO_URL, commit_hash))
-f.close()
+REPO_URL = 'https://github.com/sulab/WikidataIntegrator'
 
 setup(
     name='wikidataintegrator',
@@ -52,7 +17,6 @@ setup(
     license='MIT',
     keywords='Wikidata biology chemistry medicine',
     url=REPO_URL,
-    download_url='https://github.com/sebotic/WikidataIntegrator/tarball/0.0.325',
     # packages=find_packages(),
     packages=['wikidataintegrator', 'wikidataintegrator.backoff', 'wikidataintegrator.ref_handlers'],
     include_package_data=True,
