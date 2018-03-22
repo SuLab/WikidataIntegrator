52d5f1fb7c360cbb32ed9fcd9115671a7950ba63
Mahdi Moqri <moqri@ufl.edu>
2018-03-22 17:28:13 -0400

    Delete example1.py

--- example1.py
+++
@@ -1,6 +0,0 @@
-from wikidataintegrator import wdi_core
-
-my_first_wikidata_item = wdi_core.WDItemEngine(wd_item_id='Q5')
-
-# to check successful installation and retrieval of the data, you can print the json representation of the item
-print my_first_wikidata_item.get_wd_json_representation()
