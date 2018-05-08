3c9d9c66a5b4271648e88f067ef1e1ef1a94eaba
Greg Stupp <gstupp@scripps.edu>
2018-05-08 15:42:29 -0700

    add a WikibaseHelper for dealing with equivalent classes and items across wikibases

--- wikidataintegrator/wdi_helpers.py
+++ wikidataintegrator/wdi_helpers.py
@@ -21,6 +21,66 @@ RELATIONS = {'close': 'Q39893184',
              'narrow': 'Q39893967'}
 
 
+class WikibaseHelper:
+    """
+    Helper functions for accessing PIDs and QIDs across wikibases
+
+    This functionality depends upon their existing two properties: equivalent property and equivalen class
+    with the equivalent property values: 'http://www.w3.org/2002/07/owl#equivalentProperty' and
+    'http://www.w3.org/2002/07/owl#equivalentClass' respectively
+    """
+
+    def __init__(self, sparql_endpoint_url='https://query.wikidata.org/sparql'):
+        self.sparql_endpoint_url = sparql_endpoint_url
+        # a map of property URIs to a PID in the wikibase you are using
+        uri_pid = id_mapper(self.guess_equivalent_property_pid(), endpoint=self.sparql_endpoint_url,
+                            return_as_set=True)
+        # remove duplicates/conflicts
+        self.URI_PID = {k: list(v)[0] for k, v in uri_pid.items() if len(v) == 1}
+        # get equivalent class PID
+        equiv_class_pid = self.URI_PID['http://www.w3.org/2002/07/owl#equivalentClass']
+        # a map of item URIs to a QID in the wikibase you are using
+        uri_qid = id_mapper(equiv_class_pid, endpoint=self.sparql_endpoint_url,
+                            return_as_set=True)
+        # remove duplicates/conflicts
+        self.URI_QID = {k: list(v)[0] for k, v in uri_qid.items() if len(v) == 1}
+
+    def guess_equivalent_property_pid(self):
+        # get the equivalent property PID without knowing the PID for equivalent property!!!
+        query = '''SELECT * WHERE {
+          ?item ?prop <http://www.w3.org/2002/07/owl#equivalentProperty> .
+          ?item <http://wikiba.se/ontology#directClaim> ?prop .
+        }'''
+        pid = wdi_core.WDItemEngine.execute_sparql_query(query, endpoint=self.sparql_endpoint_url)
+        pid = pid['results']['bindings'][0]['prop']['value']
+        pid = pid.split("/")[-1]
+        return pid
+
+    def get_pid(self, uri):
+        """
+        Get the pid for the property in this wikibase instance ( the one at `sparql_endpoint_url` ),
+         that corresponds to (i.e. has the equivalent property) `uri`
+        """
+        # if the wikibase is wikidata, and we give a wikidata uri or a PID with no URI specified:
+        if (self.sparql_endpoint_url == 'https://query.wikidata.org/sparql' and
+                (uri.startswith("P") or uri.startswith("http://www.wikidata.org/entity/"))):
+            # don't look up anything, just return the same value
+            return uri
+        if uri.startswith("P"):
+            uri = "http://www.wikidata.org/entity/" + uri
+        return self.URI_PID[uri]
+
+    def get_qid(self, uri):
+        # if the wikibase is wikidata, and we give a wikidata uri or a QID with no URI specified:
+        if (self.sparql_endpoint_url == 'https://query.wikidata.org/sparql' and
+                (uri.startswith("Q") or uri.startswith("http://www.wikidata.org/entity/"))):
+            # don't look up anything, just return the same value
+            return uri
+        if uri.startswith("Q"):
+            uri = "http://www.wikidata.org/entity/" + uri
+        return self.URI_QID[uri]
+
+
 class Release(object):
     """
     Create a release item
@@ -260,9 +320,9 @@ class PubmedItem(object):
         self.meta['pmid'] = self.article.get('pmid', '')
         self.meta['pmcid'] = self.article.get('pmcid', '').replace("PMC", "")
         self.meta['title'] = self.article['title'][:249]
-        original_title=self.meta['title'] # to remove trailing dot
-        if original_title[-1] == '.' and original_title[-3] != '.': # to exclude abbreviations such as U.S.A.
-            self.meta['title'] = original_title[:-1] # drop the trailing dot
+        original_title = self.meta['title']  # to remove trailing dot
+        if original_title[-1] == '.' and original_title[-3] != '.':  # to exclude abbreviations such as U.S.A.
+            self.meta['title'] = original_title[:-1]  # drop the trailing dot
 
         if 'pubTypeList' in self.article:
             pubtypes = self.article['pubTypeList']['pubType']
