e10739237cbbf5519fdd22f1ebf5065c0977b95f
andrawaag <andra@waagmeester.net>
2019-01-07 12:19:50 +0100

    * Changed the python version to 3.6 * Added a README stub on the implemented ShEx function in WDI-core * change the functionname to check_shex_conformence

--- wikidataintegrator/wdi_core.py
+++ wikidataintegrator/wdi_core.py
@@ -1214,7 +1214,7 @@ class WDItemEngine(object):
         return df
 
     @staticmethod
-    def check_shex_conformity(item_iri, shex, endpoint="https://query.wikidata.org/sparql"):
+    def check_shex_conformence(item_iri, shex, endpoint="https://query.wikidata.org/sparql"):
         """
             Static method which can be used to check conformance of an item to a schema provided as a Shape Expression
             :param item_iri: The full iri of a Wikidata item to test for conformity
