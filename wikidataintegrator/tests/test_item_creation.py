e5865fdb52e18c0cf1057eced415d6893982e85e
andrawaag <andra@waagmeester.net>
2019-01-21 20:17:40 +0100

    Adapt the test to the removal of domain and iten name

--- wikidataintegrator/tests/test_item_creation.py
+++ wikidataintegrator/tests/test_item_creation.py
@@ -23,12 +23,12 @@ class TestItemCreation(unittest.TestCase):
         core_props = set(["P{}".format(x) for x in range(20)])
 
         for d in data:
-            item = wdi_core.WDItemEngine(data=[d], core_props=core_props)
+            item = wdi_core.WDItemEngine(new_item=True, data=[d], core_props=core_props)
             assert item.get_wd_json_representation()
-            item = wdi_core.WDItemEngine(data=[d], core_props=set())
+            item = wdi_core.WDItemEngine(new_item=True, data=[d], core_props=set())
             assert item.get_wd_json_representation()
 
-        item = wdi_core.WDItemEngine(data=data, core_props=core_props)
+        item = wdi_core.WDItemEngine(new_item=True, data=data, core_props=core_props)
         assert item.get_wd_json_representation()
-        item = wdi_core.WDItemEngine(data=data, core_props=set())
+        item = wdi_core.WDItemEngine(new_item=True, data=data, core_props=set())
         assert item.get_wd_json_representation()
