8826fdbe35284732c4001616167aefbc18bbe3f3
andrawaag <vLMgjeXynn6WA5p8T&Q2D>
2021-01-30 10:10:15 +0100

    version release

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     include_package_data=True,
     ## long_description=read('README.md'),
     classifiers=[
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
