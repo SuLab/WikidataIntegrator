e425e7037e0dfffc4ab92feede5a8f81637ec2a4
Andra Waagmeester <andra@micelio.be>
2021-05-19 21:45:21 +0200

    version 0.8.18

--- wikidataintegrator/wdi_config.py
+++ wikidataintegrator/wdi_config.py
@@ -34,3 +34,33 @@ config = {
     'WIKIBASE_URL': 'http://www.wikidata.org',
     'CONCEPT_BASE_URI': 'http://www.wikidata.org/entity/'
 }
+
+prefix = {
+    'dct' : 'http://purl.org/dc/terms/',
+	'rdfs' : 'http://www.w3.org/2000/01/rdf-schema#',
+	'owl' : 'http://www.w3.org/2002/07/owl#',
+	'wikibase' : 'http://wikiba.se/ontology#',
+	'skos' : 'http://www.w3.org/2004/02/skos/core#',
+	'schema' : 'http://schema.org/',
+	'cc' : 'http://creativecommons.org/ns#',
+	'geo' : 'http://www.opengis.net/ont/geosparql#',
+	'prov' : 'http://www.w3.org/ns/prov#',
+	'wd' : 'http://www.wikidata.org/entity/',
+	'data' : 'https://www.wikidata.org/wiki/Special:EntityData/',
+	's' : 'http://www.wikidata.org/entity/statement/',
+	'ref' : 'http://www.wikidata.org/reference/',
+	'v' : 'http://www.wikidata.org/value/',
+	'wdt' : 'http://www.wikidata.org/prop/direct/',
+	'wdtn' : 'http://www.wikidata.org/prop/direct-normalized/',
+	'p' : 'http://www.wikidata.org/prop/',
+	'ps' : 'http://www.wikidata.org/prop/statement/',
+	'psv' : 'http://www.wikidata.org/prop/statement/value/',
+	'psn' : 'http://www.wikidata.org/prop/statement/value-normalized/',
+	'pq' : 'http://www.wikidata.org/prop/qualifier/',
+	'pqv' : 'http://www.wikidata.org/prop/qualifier/value/',
+	'pqn' : 'http://www.wikidata.org/prop/qualifier/value-normalized/',
+	'pr' : 'http://www.wikidata.org/prop/reference/',
+	'prv' : 'http://www.wikidata.org/prop/reference/value/',
+	'prn' : 'http://www.wikidata.org/prop/reference/value-normalized/',
+	'wdno' : 'http://www.wikidata.org/prop/novalue/',
+}
