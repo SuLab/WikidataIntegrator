de6912a62ace7cda0c53c809a783a0a8fd6fdd31
Andra Waagmeester <andra@micelio.be>
2022-02-06 20:36:53 +0100

    first draft of sdc support

--- wikidataintegrator/__init__.py
+++ wikidataintegrator/__init__.py
@@ -4,7 +4,7 @@ import wikidataintegrator.wdi_core
 import wikidataintegrator.wdi_fastrun
 import wikidataintegrator.wdi_helpers
 import wikidataintegrator.wdi_login
-import wikidataintegrator.sdc_cores
+import wikidataintegrator.sdc_core
 
 try:
     __version__ = pkg_resources.get_distribution("wikidataintegrator").version
